696ee4134257eb00d402a65edb03cec0d21debdd
Nathan Flurry <developer@nathanflurry.com>
2023-01-25 10:46:46 +0000

    Auto-generate API endpoints

---
+++ bin/api/gen.ts
@@ -0,0 +1,76 @@
+#!/usr/bin/env -S deno run --allow-env --allow-read --allow-write
+
+import yaml from "npm:yaml";
+import { emptyDir } from "https://deno.land/std/fs/mod.ts";
+
+const spec = yaml.parse(await Deno.readTextFile("openapi.yml"));
+let mintJson = JSON.parse(await Deno.readTextFile("mint.json"));
+
+let importantEndpoints = {
+	"matchmaker": [
+		"POST /lobbies/find",
+		"POST /lobbies/join",
+		"POST /lobbies/ready",
+		"POST /players/connected",
+		"POST /players/disconnected",
+	],
+	"identity": [
+		"POST /identities",
+		"POST /game-links",
+		"POST /game-links/complete",
+		"GET /game-links/complete",
+	],
+};
+
+let apiTemplates = {};
+for (let navigation of mintJson.navigation) {
+	if (navigation.__apiTemplate) {
+		apiTemplates[navigation.__apiTemplate] = navigation;
+		navigation.pages = [];
+	}
+}
+
+await emptyDir("matchmaker/api");
+await emptyDir("identity/api");
+
+for (let pathName in spec.paths) {
+	for (let method in spec.paths[pathName]) {
+		let path = spec.paths[pathName][method];
+
+		console.log('Registering', method, pathName);
+
+		// TODO: Hack
+		let product;
+		if (pathName.startsWith("/lobbies") || pathName.startsWith("/players")) {
+			product = "matchmaker";
+		} else {
+			product = "identity";
+		}
+
+		let indexableName = `${method.toUpperCase()} ${pathName}`;
+		let isImportant = importantEndpoints[product].indexOf(indexableName) != -1;
+
+		let title = `${method.toUpperCase()} ${pathName}`;
+		if (isImportant) title = "⭐️ " + title;
+
+		let file = `---\ntitle: "${title}"\nopenapi: "${method.toUpperCase()} ${path}"\n---`;
+
+		let pathStripped = pathName.replace(/\/\{.*\}/g, "").replace(/\//g, "-");
+		let filePath = new String(`${product}/api/${method}${pathStripped}`);
+
+		// Sort by grouping similar endpoints together and move important endpionts first
+		filePath.sortingKey = `${isImportant ? "0" : "9"} ${pathName} ${method}`;  
+
+		await Deno.writeTextFile(`${filePath}.mdx`, file);
+
+		apiTemplates[product].pages.push(filePath);
+		apiTemplates[product].pages.sort((a, b) => {
+			if (a.sortingKey < b.sortingKey) return -1;
+			else if (a.sortingKey > b.sortingKey) return 1;
+			else return 0;
+		});
+
+	}
+}
+
+await Deno.writeTextFile("mint.json", JSON.stringify(mintJson, null, 4));
